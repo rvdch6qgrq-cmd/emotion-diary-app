0c6112a7138f4bdc787fbe20c438b8d981806c0b
rvdch6qgrq-cmd <rvdch6qgrq@privaterelay.appleid.com>
2026-05-27 16:08:41 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
 
   return (
     <div className="app">
-      <h1>Дневникэмоций</h1>
+      <h1>Дневник эмоций</h1>
 
       <p className="subtitle">Как ты сейчас себя чувствуешь?</p>
 
